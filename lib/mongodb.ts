c068174a752849e7f8dca069f38e43d83f428ab4
Wallace W. <gameloverguy513@gmail.com>
2026-03-23 18:12:54 +1100

    removed and refactor files for netlify deploy

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
-}
-
 /*
 Global cache prevents multiple connections in development
 */
@@ -29,6 +23,12 @@ if (!global.mongooseCache) {
 }
 
 export async function connectDB() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable");
+  }
+  
   if (cached.conn) {
     return cached.conn;
   }
