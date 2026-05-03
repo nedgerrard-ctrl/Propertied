e7c2f0b545ea2578bdd78e921902afabf42a9a58
bryan <bsoe0002@student.monash.edu>
2026-05-03 20:11:14 +1000

    removed edit pages button

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -28,6 +28,8 @@ export default async function AdminDashboardPage() {
             </p>
           </div>
 
+  
+
           <div className="flex items-center gap-3">
             <Link
               href="/admin/dashboard/pages/new"
@@ -47,7 +49,7 @@ export default async function AdminDashboardPage() {
             >
               Manage Blogs
             </Link>
-                        <EditPagesButton />
+                      
             <LogoutButton />
           </div>
         </div>
